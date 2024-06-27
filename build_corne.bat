1f3834d8b5d6a290fe3a2a559aeecfde704c9ed0
daniel2887 <daniels2887@gmail.com>
2024-06-26 22:40:11 -0700

    Cosmetic changes to build_corne.bat

--- build_corne.bat
+++ build_corne.bat
@@ -1,7 +1,15 @@
 @echo off
 pushd app
-west build -d build/left -b nice_nano_v2 -- -DSHIELD=corne_left -DSHIELD=corne_left -DZMK_CONFIG="../../zmk-config/config" || echo Error building Corne left && popd && exit /b
-west build -d build/right -b nice_nano_v2 -- -DSHIELD=corne_right -DSHIELD=corne_left -DZMK_CONFIG="../../zmk-config/config" || echo Error building Corne right && popd && exit /b
+echo.
+echo -----------------------
+echo Now building Corne left
+echo -----------------------
+west build -d build/left -b nice_nano_v2 -- -DSHIELD=corne_left -DZMK_CONFIG="../../zmk-config/config" || echo Error building Corne left && popd && exit /b
+echo.
+echo -----------------------
+echo Now building Corne right
+echo -----------------------
+west build -d build/right -b nice_nano_v2 -- -DSHIELD=corne_right -DZMK_CONFIG="../../zmk-config/config" || echo Error building Corne right && popd && exit /b
 popd
 if not exist "output" mkdir output
 copy app\build\left\zephyr\zmk.uf2 output\left.uf2 || echo Error copying right firmware && exit /b
