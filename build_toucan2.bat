60d2f917c3defcf5a8c54569f13cecc81894ee04
daniel2887 <daniels2887@gmail.com>
2026-08-22 14:07:44 -0700

    fix(build): pass EXTRA_CFLAGS and EXTRA_CXXFLAGS to prevent LVGL G drive conflict

--- build_toucan2.bat
+++ build_toucan2.bat
@@ -2,8 +2,7 @@
 setlocal
 
 :: Prevent drive letter G macro expansion conflicts in Zephyr v3.5.0 LVGL
-set "CFLAGS=-ULV_CONF_PATH"
-set "CXXFLAGS=-ULV_CONF_PATH"
+set "EXTRA_FLAGS=-DEXTRA_CFLAGS=-ULV_CONF_PATH -DEXTRA_CXXFLAGS=-ULV_CONF_PATH"
 
 :: Get the directory where the script is located (absolute path with trailing backslash)
 set "SCRIPT_DIR=%~dp0"
@@ -24,14 +23,13 @@ for %%i in ("%SCRIPT_DIR%..\zmk-keyboard-toucan2") do set "TOUCAN_MODULE=%%~fi"
 set "TOUCAN_MODULE=%TOUCAN_MODULE:\=/%"
 set "TOUCAN_CONFIG=%TOUCAN_MODULE%/config"
 
-:: Resolve repos outside my_repos (assuming they are in a sibling 'repos' directory to 'my_repos')
-for %%i in ("%SCRIPT_DIR%..\..\repos\zmk-rgbled-widget") do set "RGB_MODULE=%%~fi"
+for %%i in ("%SCRIPT_DIR%..\..\upstream\zmk-rgbled-widget") do set "RGB_MODULE=%%~fi"
 set "RGB_MODULE=%RGB_MODULE:\=/%"
-for %%i in ("%SCRIPT_DIR%..\..\repos\zmk_driver_azoteq") do set "AZOTEQ_MODULE=%%~fi"
+for %%i in ("%SCRIPT_DIR%..\zmk_driver_azoteq") do set "AZOTEQ_MODULE=%%~fi"
 set "AZOTEQ_MODULE=%AZOTEQ_MODULE:\=/%"
-for %%i in ("%SCRIPT_DIR%..\..\repos\zmk-input-zoom") do set "ZOOM_MODULE=%%~fi"
+for %%i in ("%SCRIPT_DIR%..\..\upstream\zmk-input-zoom") do set "ZOOM_MODULE=%%~fi"
 set "ZOOM_MODULE=%ZOOM_MODULE:\=/%"
-for %%i in ("%SCRIPT_DIR%..\..\repos\zmk-pointing-acceleration") do set "ACCEL_MODULE=%%~fi"
+for %%i in ("%SCRIPT_DIR%..\zmk-pointing-acceleration") do set "ACCEL_MODULE=%%~fi"
 set "ACCEL_MODULE=%ACCEL_MODULE:\=/%"
 
 :: Create output directory immediately so it's ready for the first-finisher
@@ -39,8 +37,8 @@ if not exist "%SCRIPT_DIR%output" mkdir "%SCRIPT_DIR%output"
 
 :: Define the base build commands
 set "EXTRA_MODULES=%TOUCAN_MODULE%;%RGB_MODULE%;%AZOTEQ_MODULE%;%ZOOM_MODULE%;%ACCEL_MODULE%"
-set "LEFT_BUILD=west build --pristine=auto -b seeeduino_xiao_ble -d build/left -- -DSHIELD="toucan_left rgbled_adapter nice_view_gem" -DZMK_CONFIG="%TOUCAN_CONFIG%" -DZMK_EXTRA_MODULES="%EXTRA_MODULES%""
-set "RIGHT_BUILD=west build --pristine=auto -b seeeduino_xiao_ble -d build/right -- -DSHIELD="toucan_right rgbled_adapter" -DZMK_CONFIG="%TOUCAN_CONFIG%" -DZMK_EXTRA_MODULES="%EXTRA_MODULES%""
+set "LEFT_BUILD=west build --pristine=auto -b seeeduino_xiao_ble -d build/left -- %EXTRA_FLAGS% -DSHIELD="toucan_left rgbled_adapter nice_view_gem" -DZMK_CONFIG="%TOUCAN_CONFIG%" -DZMK_EXTRA_MODULES="%EXTRA_MODULES%""
+set "RIGHT_BUILD=west build --pristine=auto -b seeeduino_xiao_ble -d build/right -- %EXTRA_FLAGS% -DSHIELD="toucan_right rgbled_adapter" -DZMK_CONFIG="%TOUCAN_CONFIG%" -DZMK_EXTRA_MODULES="%EXTRA_MODULES%""
 
 
 pushd "%SCRIPT_DIR%app"
